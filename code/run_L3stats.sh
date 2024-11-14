049fbb62b75debd946c3a319fbb0a77866b0f1d4
jameswyngaarden <james.wyngaarden@temple.edu>
2024-11-14 11:06:09 -0500

    run act for eps models

--- code/run_L3stats.sh
+++ code/run_L3stats.sh
@@ -10,13 +10,14 @@ maindir="$(dirname "$scriptdir")"
 
 
 # this loop defines the different types of analyses that will go into the group comparisons
-for analysis in nppi-dmn nppi-ecn; do #act; do # nppi-dmn nppi-ecn; do
+for analysis in act; do #nppi-dmn nppi-ecn; do #act; do # nppi-dmn nppi-ecn; do
 	analysistype=type-${analysis}
 
 	# these define the cope number (copenum) and cope name (copename)
 	# "1 comp" "2 comp_p" "3 in" "4 in_p" "5 out" "6 out_p"
 	# "7 in_p-out_p" "8 soc_p-nonsoc_p" "9 in-out" "10 soc-nonsoc" "11 phys"
-	for copeinfo in "1 comp" "2 comp_p" "3 in" "4 in_p" "5 out" "6 out_p" "7 in_p-out_p" "8 soc_p-nonsoc_p" "9 in-out" "10 soc-nonsoc" "11 phys"; do
+	#for copeinfo in "1 comp" "2 comp_p" "3 in" "4 in_p" "5 out" "6 out_p" "7 in_p-out_p" "8 soc_p-nonsoc_p" "9 in-out" "10 soc-nonsoc" "11 phys"; do
+	for copeinfo in "7 in_p-out_p" "8 soc_p-nonsoc_p" "9 in-out" "10 soc-nonsoc" "11 phys"; do
 
 		# split copeinfo variable
 		set -- $copeinfo
