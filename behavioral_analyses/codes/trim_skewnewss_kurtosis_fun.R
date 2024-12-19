94dcacb0a171c95523f9a8bac4e18e65676b318d
jen-yang <31789285+jen-yang@users.noreply.github.com>
2024-12-19 18:52:02 -0500

    trim code update

--- behavioral_analyses/codes/trim_skewnewss_kurtosis_fun.R
+++ behavioral_analyses/codes/trim_skewnewss_kurtosis_fun.R
@@ -6,11 +6,12 @@ trim_for_kurtosis <- function(raw_vector,
   # raw_vector = sub_i_fit$alpha_i
   raw_vector = sub_i_fit$epsilon_i
   target_kurtosis = 0.1
-  target_skew = 0.5
-  target_skew = 1
+  target_skew = 0.2
   tolerance = 0.05
   trim_step = 1
   ###
+  
+  sorted_vector = sort(raw_vector)
   
   n <- length(raw_vector)
   lower_bound <- 1
@@ -23,8 +24,8 @@ trim_for_kurtosis <- function(raw_vector,
   while (TRUE) {
     trimmed_vector <- sorted_vector[lower_bound:upper_bound]
     
-    # current_kurtosis <- kurtosis(trimmed_vector)
-    # distance_kurtosis_target = abs(current_kurtosis - target_kurtosis)
+    current_kurtosis <- kurtosis(trimmed_vector)
+    distance_kurtosis_target = abs(current_kurtosis - target_kurtosis)
 
     current_skewness <- skewness(trimmed_vector)
     distance_skewness_target = abs(abs(current_skewness) - target_skew)
@@ -32,28 +33,27 @@ trim_for_kurtosis <- function(raw_vector,
     print(sprintf("skewness = %.2f, kurtosis = %.2f", 
                   current_skewness, current_kurtosis))
     
-    if (distance_skewness_target <= tolerance & distance_kurtosis_target <= tolerance) {
+    if (distance_skewness_target <= tolerance) {
       break
     }
+    # first trim skewness
     ###
-    if (current_kurtosis > target_kurtosis) {
-      ###
-      if (current_skewness< -1) {
-        lower_bound <- lower_bound + trim_step*2 
-        upper_bound <- upper_bound - trim_step
-      } else if (current_skewness > 1) {
-        lower_bound <- lower_bound + trim_step
-        upper_bound <- upper_bound - trim_step*2
-      } else {
-        lower_bound <- lower_bound + trim_step 
-        upper_bound <- upper_bound - trim_step
-      }
-      ###
+    if (current_skewness < 0 & current_skewness < -target_skew) {
+      lower_bound <- lower_bound + trim_step
+      upper_bound <- upper_bound
+    } else if (current_skewness > 0 & current_skewness > target_skew) {
+      lower_bound <- lower_bound
+      upper_bound <- upper_bound - trim_step
     } else {
       break
     }
     ###
     if (lower_bound >= upper_bound) break
+    
+    ####### decrease kurtosis, to continue!
+    
+    
+    
   }
   histogram(trimmed_vector)
   return(trimmed_vector)
